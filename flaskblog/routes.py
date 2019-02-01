98a290b0d9119f2763231ed3746ec0447fae003f
zeus0789 <abhishek.tiwari3507@gmail.com>
2019-02-01 23:54:44 +0530

    login routes fixed

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,8 @@
 from flaskblog.models import User, Post
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from flaskblog.form import RegistrationForm, LoginForm
 from flaskblog import app, db, bcrypt
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 data = [
 	{
@@ -61,6 +61,8 @@ def login():
 		user = User.query.filter_by(email=form.email.data).first()
 		if user and bcrypt.check_password_hash(user.password, form.password.data)==True:
 			login_user(user, remember=form.remember.data)
+			next_page = request.args.get('next')
+			return redirect(next_page) if next_page else redirect(url_for('home'))
 		else:
 			flash("Login unsuccessful. Check email/password",'danger')
 	return render_template("login.html", title="Login", form=form)
@@ -73,3 +75,9 @@ def forgot_password():
 def logout():
 	logout_user()
 	return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+	return render_template('account.html', title='Account')
