6e192dcf980e724d0ffb7d32da1e19fac7cd8bcb
zeus0789 <abhishek.tiwari3507@gmail.com>
2019-02-02 15:14:34 +0530

    Account page added

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -11,7 +11,7 @@ class User(db.Model, UserMixin):
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=False)
 	password = db.Column(db.String(60), nullable=False)
-	image_file = db.Column(db.String(20),nullable=False, default='default.jpg')
+	image_file = db.Column(db.String(20),nullable=False, default='profile.png')
 	posts = db.relationship('Post',backref='author',lazy=True)
 
 	def __repr__(self):
